6e839105d4a74c2bc45c099a8e497ced9943c02e
jlvdb <jlvdb@astro.uni-bonn.de>
2017-12-13 23:24:22 +0100

    added "if __name__ == '__main__'" to executables

--- theli.py
+++ theli.py
@@ -3,19 +3,24 @@ from system.reduction import Reduction
 from commandline.parser import Parser
 
 
-args, joblist, theli_args = Parser.parse_theli_args()
+def main():
+    args, joblist, theli_args = Parser.parse_theli_args()
 
-project = Reduction(
-    args.inst, args.main, title=args.title,
-    biasdir=args.bias, darkdir=args.dark, flatdir=args.flat,
-    flatoffdir=args.flatoff, sciencedir=args.science, skydir=args.sky,
-    stddir=args.standard, reduce_skydir=args.reduce_sky,
-    ncpus=args.threads, verbosity=args.verbosity, parseparams=theli_args,
-    logdisplay=args.log_display, check_filters=args.disable_filter_check,
-    ignore_weight_timestamp=args.ignore_weight_timestamp)
+    project = Reduction(
+        args.inst, args.main, title=args.title,
+        biasdir=args.bias, darkdir=args.dark, flatdir=args.flat,
+        flatoffdir=args.flatoff, sciencedir=args.science, skydir=args.sky,
+        stddir=args.standard, reduce_skydir=args.reduce_sky,
+        ncpus=args.threads, verbosity=args.verbosity, parseparams=theli_args,
+        logdisplay=args.log_display, check_filters=args.disable_filter_check,
+        ignore_weight_timestamp=args.ignore_weight_timestamp)
 
-for job in joblist:
-    # read parameters for Reduction - classmethods
-    jobargs = [getattr(args, param) for param in job["para"]]
-    # execute job
-    getattr(project, job["func"])(*jobargs)
+    for job in joblist:
+        # read parameters for Reduction - classmethods
+        jobargs = [getattr(args, param) for param in job["para"]]
+        # execute job
+        getattr(project, job["func"])(*jobargs)
+
+
+if __name__ == '__main__':
+    main()
